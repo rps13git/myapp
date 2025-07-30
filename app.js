202095a58354c5854f631b156a0cdfde3042a90d
rps13git <rps13goto@yahoo.co.jp>
2025-07-30 19:13:36 +0900

    first commit

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var helloRouter = require('./routes/hello');
+var notesRouter = require('./routes/notes');
+var catRouter = require('./routes/cat');
+var notes_from_bRouter = require('./routes/notes_from_b');
 
 var app = express();
 
@@ -23,6 +26,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/hello', helloRouter);
+app.use('/notes', notesRouter);
+app.use('/cat', catRouter);
+app.use('/notes_from_b', notes_from_bRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- app2.js
+++
@@ -1,8 +0,0 @@
-const http = require ('http');
-
-var server = http. createServer(
-  (request,response)=>{
-    response.end('Hello Node.js!');
-  }
-);
-server.listen(3000);
